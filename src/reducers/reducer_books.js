347ef0ff8c8f7438747f4d94069cb04ccfc83c02
Quang-Che <quang.n.che@gmail.com>
2017-10-12 15:59:32 -0700

    feat: Added Redux utilities to the application
    This commit includes a reducer function for the application and wires
    up the reducer to the application using Redux and React-Redux. A
    container has also been created to bring together Redux and React
    in order to manage the state and render the view of the application.
    
    TODO: Implement React-Redux to link up the container with Redux to
    inject data into the container for the whole application to start
    managing a piece of state using Redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: "50 Shades of Grey"},
+        {title: "The Great Gatsby"},
+        {title: "JavaScript: The Good Parts"},
+        {title: "Twilight"}
+    ]
+}
